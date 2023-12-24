575a9b71e091ba9318e45bff643cd7245baf3057
Ekansh Kandulna <efuforofficial@gmail.com>
2023-12-24 20:31:31 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ async function fetchData() {
         method: 'POST',
         url: 'https://anilist-graphql.p.rapidapi.com/',
         headers: {
-            'x-rapidapi-key': '2b7dad3861msh3c855ef1275cf78p1df61ajsn281baa842d1b',
+            'x-rapidapi-key': '',
             'x-rapidapi-host': 'anilist-graphql.p.rapidapi.com',
             'Content-Type': 'application/json'
         },
